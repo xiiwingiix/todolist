3e88793a529e610cd2ad5e548b54427ae13f00f4
xiiwingiix <xiiwingiix@daum.net>
2020-01-28 15:23:41 +0900

    init

--- js/todolist.js
+++ js/todolist.js
@@ -1,5 +1,6 @@
 var insert_todo = document.querySelector('.input_todo');
 var list_todo		= document.querySelector('.todoList');
+var list_done		= document.querySelector('.doneList');
 var btn_delete 	= '';
 
 window.onload = function(){
@@ -7,12 +8,16 @@ window.onload = function(){
 
 		if (e.keyCode === 13 && insert_todo.value != ''){
 
-			var ele_todo 	= document.createElement('li');
-			var ele_btn 	= document.createElement('span');
+			var ele_todo = document.createElement('li');
+			var ele_btn  = document.createElement('span');
 
-			ele_todo.textContent 	= insert_todo.value;
-			ele_btn.className			= 'btn_delete'
-			ele_btn.textContent 	= '×'
+			//todoList 
+			ele_todo.textContent = insert_todo.value;
+			ele_todo.addEventListener('click', doneList);
+
+			//delete button
+			ele_btn.className		= 'btn_delete'
+			ele_btn.textContent = '×'
 			ele_btn.addEventListener('click', deleterList);
 
 			list_todo.appendChild(ele_todo);
@@ -20,17 +25,30 @@ window.onload = function(){
 
 			insert_todo.value = '';
 
-			btn_delete = document.querySelector('.btn_delete');
-			console.log(btn_delete);
 
 		}
 	});
 
+	//delete fuction
 	function deleterList(e){
 		if(e){
-			e.target.parentElement.remove();
-		} else {
-			console.log('논클릭');
-		}
+			e.stopPropagation();
+
+			var confirm_delete = confirm('삭제하시겠습니까?');
+			
+			if (confirm_delete) {
+				e.target.parentNode.remove();
+			} 
+		} 
+	}
+	
+	//done move function
+	function doneList(e){
+		if(e){
+			var confirm_move = confirm('Done list로 이동하겠습니까?');
+			if ( confirm_move ) {
+				list_done.appendChild(e.target);
+			}
+		} 
 	}
 }
